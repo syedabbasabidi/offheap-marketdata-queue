b17829fe53c892c735b74d69361084db6a2ffc53
Abbas <monazaidi5@gmail.com>
2023-02-20 21:11:43 +0000

    Dont use String.toBytes

--- src/main/java/marketdata/producer/CircularQueueProducer.java
+++ src/main/java/marketdata/producer/CircularQueueProducer.java
@@ -4,22 +4,58 @@ import model.MarketData;
 import queue.CircularMMFQueue;
 
 import java.io.IOException;
-
-import static java.lang.System.nanoTime;
+import java.lang.management.BufferPoolMXBean;
+import java.lang.management.ManagementFactory;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class CircularQueueProducer {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         MarketData marketData = new MarketData();
         CircularMMFQueue mmfQueue = CircularMMFQueue.getInstance(marketData.size());
 
-        long startTime = nanoTime();
-        int j  = 0;
-        while(true) {
-            marketData.set("GB00BJLR0J16", 101.12d + j, j % 2 == 0 ? 0 : 1, j % 2 == 0, (byte)1, "BRC", "2022-09-14:22:10:13");
+        new Thread(() -> {
+            try {
+                logMem();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+
+        int j = 0;
+        marketData. set("GB00BJLR0J16", 101.12d + j, 0, true, (byte) 1, "BRC", "2022-09-14:22:10:13");
+        while (true) {
+            marketData.setPrice(101.12d + j);
+            marketData.side(j % 2 == 0 ? 0 : 1);
+            marketData.setFirm(j % 2 == 0);
             mmfQueue.add(marketData.getData());
             j++;
+            if (j % 5000 == 0) {
+                Thread.sleep(10);
+            }
         }
+    }
+
+    private static void logMem() throws InterruptedException {
+
+        while (true) {
+            List<BufferPoolMXBean> pools = ManagementFactory.getPlatformMXBeans(BufferPoolMXBean.class);
+            for (BufferPoolMXBean pool : pools) {
+                System.out.println(pool.getName());
+                System.out.println(pool.getCount());
+                System.out.println(LocalDateTime.now() +"memory used " + toMB(pool.getMemoryUsed()));
+                System.out.println("total capacity" + toMB(pool.getTotalCapacity()));
+            }
+            System.out.println("heap mem used: "+toMB(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()));
+            System.out.println("heap mem total: "+toMB(Runtime.getRuntime().maxMemory()));
+            System.out.println();
+            Thread.sleep(1000);
+        }
+    }
+
+    private static String toMB(long init) {
+        return (Long.valueOf(init).doubleValue() / (1024 * 1024)) + " MB";
     }
 }
--- src/main/java/model/MarketData.java
+++ src/main/java/model/MarketData.java
@@ -2,6 +2,8 @@ package model;
 
 import util.ByteUtils;
 
+import java.nio.ByteBuffer;
+
 import static java.util.Arrays.copyOfRange;
 import static util.ByteUtils.bytesToLong;
 import static util.ByteUtils.longToBytes;
@@ -11,6 +13,10 @@ public class MarketData {
     private static final int OBJ_SIZE = 45;
     private volatile byte[] data;
 
+    private final ByteBuffer securityMapper = ByteBuffer.allocateDirect(12);
+    private final ByteBuffer brokerMapper = ByteBuffer.allocateDirect(3);
+    private final ByteBuffer dateMapper = ByteBuffer.allocateDirect(19);
+
     public MarketData() {
         data = new byte[OBJ_SIZE];
     }
@@ -18,36 +24,53 @@ public class MarketData {
     public void setData(byte[] data) {
         this.data = data;
     }
+
     public void setFirm(boolean isFirm) {
         data[0] = (byte) (isFirm ? 1 : 0);
     }
+
     public void side(int buy) {
         data[1] = (byte) buy;
     }
+
     public void setSecurity(String id) {
-        byte[] bytes = id.getBytes();
-        for (int i =0,j=2; i < bytes.length; i++,j++) {
-            data[j] = bytes[i];
+        for(int i=0; i<id.length(); i++) {
+            securityMapper.put((byte) id.charAt(i));
+        }
+        for (int i = 0; i < 12; i++) {
+            data[i] = securityMapper.get();
         }
     }
+
     public void setPrice(double price) {
         byte[] bytes = longToBytes((long) (price * 1000));
-        for (int i =0,j=14; i < bytes.length; i++,j++) {
+        for (int i = 0, j = 14; i < bytes.length; i++, j++) {
             data[j] = bytes[i];
         }
     }
+
     public void setExpiresAt(String expiresAt) {
-        byte[] bytes = expiresAt.getBytes();
-        for (int i =0,j=22; i < bytes.length; i++,j++) {
-            data[j] = bytes[i];
+
+        for(int i=0; i<expiresAt.length(); i++) {
+            dateMapper.put((byte) expiresAt.charAt(i));
+        }
+
+        for (int i = 0, j = 22; i < 19; i++, j++) {
+            data[j] = dateMapper.get();
         }
     }
+
     public void setBroker(String broker) {
-        byte[] bytes = broker.getBytes();
-        for (int i =0,j=41; i < bytes.length; i++,j++) {
-            data[j] = bytes[i];
+
+        for(int i=0; i<broker.length(); i++) {
+            brokerMapper.put((byte) broker.charAt(i));
+        }
+
+        for (int i = 0, j = 41; i < 3; i++, j++) {
+            data[j] = brokerMapper.get();
         }
     }
+
     public void setPriceType(byte priceType) {
         data[44] = priceType;
     }
@@ -56,6 +79,7 @@ public class MarketData {
     public boolean isFirm() {
         return copyOfRange(data, 0, 1)[0] == 1;
     }
+
     public int side() {
         return copyOfRange(data, 1, 2)[0];
     }
@@ -63,6 +87,7 @@ public class MarketData {
     public String getSecurity() {
         return ByteUtils.bytesToSecurity(copyOfRange(data, 2, 14));
     }
+
     public double getPrice() {
         return (double) bytesToLong(copyOfRange(data, 14, 22)) / 1000;
     }
@@ -80,8 +105,6 @@ public class MarketData {
     }
 
 
-
-
     public void set(String secId, double price, int side, boolean isFirm, byte priceType, String broker, String expiresAt) {
         this.setSecurity(secId);
         this.setPrice(price);
@@ -94,14 +117,13 @@ public class MarketData {
     }
 
 
-
     public byte[] getData() {
         return data;
     }
 
     @Override
     public String toString() {
-        return getSecurity() +"-" + getPrice() +"-"+ side() +"-"+ (isFirm() ? "Firm" : "LL") +"-"+getBroker()+"-"+ (getPriceType() == 1 ? "Price" : "Unknown")+"-"+getValidUntil();
+        return getSecurity() + "-" + getPrice() + "-" + side() + "-" + (isFirm() ? "Firm" : "LL") + "-" + getBroker() + "-" + (getPriceType() == 1 ? "Price" : "Unknown") + "-" + getValidUntil();
     }
 
     public int size() {
